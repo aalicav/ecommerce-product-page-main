38deab2cda715dfacc5a3d2465f6c39f60ca97de
aalicav <antonioalissoncavalcantedeoliv@gmail.com>
2022-01-03 17:57:15 -0300

    fotos

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let mais = document.getElementById("mais")
 let menos = document.getElementById("menos")
 let valorInicial = 125
 let quantidade = 1
+const imgs = document.getElementById("img")
+let idx = 0;
+const img = document.querySelectorAll("#img img")
 function calculaPreço(){
 
     function soma(){
@@ -23,3 +26,28 @@ function calculaPreço(){
     
 }
 calculaPreço()
+
+function carrossel(){
+    idx++;
+    if(idx > img.length){
+        idx = 0;
+    }
+    imgs.style.transform = `translateX(${-idx*500}px)`
+}
+function clickThumbnail(){
+    function mostraImagem1(evento){
+        document.querySelector("#img img").setAttribute('src',"images/image-product-1.jpg")
+    }
+    function mostraImagem2(){
+        document.querySelector("#img img").setAttribute('src',"images/image-product-2.jpg")
+    }function mostraImagem3(){
+        document.querySelector("#img img").setAttribute('src',"images/image-product-3.jpg")
+    }function mostraImagem4(){
+        document.querySelector("#img img").setAttribute('src',"images/image-product-4.jpg")
+    }
+    document.querySelector("#img_menor #thumbnail1").addEventListener('click',mostraImagem1)
+    document.querySelector("#img_menor #thumbnail2").addEventListener('click',mostraImagem2)
+    document.querySelector("#img_menor #thumbnail3").addEventListener('click',mostraImagem3)
+    document.querySelector("#img_menor #thumbnail4").addEventListener('click',mostraImagem4)
+}
+clickThumbnail()
